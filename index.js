bfbc426c6dd2c18da50b53ffb60ab4c3be13fc4b
Olamide1992 <mikaheelazeez192@gmail.com>
2023-09-11 11:17:47 -0700

    add

--- index.js
+++ index.js
@@ -1,19 +1,28 @@
-// // async function updateDateAndTime() {
-// //   try {
-// //     const response = await fetch(
-// //       "http://worldtimeapi.org/api/timezone/Africa/Lagos"
-// //     );
-// //     const data = await response.json();
-// //     const time = new Date(data.utc_datetime);
-// //     const dayOfWeek = new Intl.DateTimeFormat("en-US", {
-// //       weekday: "long",
-// //     }).format(time);
-// //     const formattedTime = time.toLocaleTimeString("en-US", {
-// //       timeZone: "Africa/Lagos",
-// //       hour: "numeric",
-// //       minute: "numeric",
-// //       second: "numeric",
-// //     });
+async function updateDateAndTime() {
+  try {
+    const response = await fetch(
+      "http://worldtimeapi.org/api/timezone/Africa/Lagos"
+    );
+    const data = await response.json();
+    const time = new Date(data.utc_datetime);
+    const dayOfWeek = new Intl.DateTimeFormat("en-US", {
+      weekday: "long",
+    }).format(time);
+    const formattedTime = time.toLocaleTimeString("en-US", {
+      timeZone: "Africa/Lagos",
+      hour: "numeric",
+      minute: "numeric",
+      second: "numeric",
+    });
+    document.querySelector(".date").textContent = dayOfWeek;
+    document.querySelector(".time").textContent = formattedTime;
+  } catch (error) {
+    console.error("Error fetching time:", error);
+  }
+}
+updateDateAndTime();
+setInterval(updateDateAndTime, 1000);
+
 // function updateUtcDateAndTime() {
 //   const now = luxon.DateTime.utc();
 //   // const now = new Date();
@@ -38,38 +47,38 @@
 // updateDateAndTime();
 // setInterval(updateUtcDateAndTime, 1000);
 
-function updateDateAndTime() {
-  const daysOfWeek = [
-    "Sunday",
-    "Monday",
-    "Tuesday",
-    "Wednesday",
-    "Thursday",
-    "Friday",
-    "Saturday",
-  ];
+// function updateDateAndTime() {
+//   const daysOfWeek = [
+//     "Sunday",
+//     "Monday",
+//     "Tuesday",
+//     "Wednesday",
+//     "Thursday",
+//     "Friday",
+//     "Saturday",
+//   ];
 
-  const now = new Date();
-  const dayOfWeek = daysOfWeek[now.getUTCDay()];
+//   const now = new Date();
+//   const dayOfWeek = daysOfWeek[now.getUTCDay()];
 
-  const options = {
-    hour: "numeric",
-    minute: "numeric",
-    second: "numeric",
-    timeZone: "UTC",
-  };
+//   const options = {
+//     hour: "numeric",
+//     minute: "numeric",
+//     second: "numeric",
+//     timeZone: "UTC"
+//   };
 
-  // const hours = now.getUTCHours();
-  // const minutes = now.getUTCMinutes();
-  // const seconds = now.getUTCSeconds();
+// const hours = now.getUTCHours();
+// const minutes = now.getUTCMinutes();
+// const seconds = now.getUTCSeconds();
 
-  // const time = `${hours.toString().padStart(2, "0")}:${minutes
-  //   .toString()
-  //   .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
-  const time = now.toLocaleTimeString("en-US", options);
+// const time = `${hours.toString().padStart(2, "0")}:${minutes
+//   .toString()
+//   .padStart(2, "0")}:${seconds.toString().padStart(2, "0")}`;
+//   const time = now.toLocaleTimeString('en-US', options);
 
-  document.querySelector(".date").textContent = dayOfWeek;
-  document.querySelector(".time").textContent = time;
-}
-updateDateAndTime();
-setInterval(updateDateAndTime, 1000);
+//   document.querySelector(".date").textContent = dayOfWeek;
+//   document.querySelector(".time").textContent = time;
+// }
+// updateDateAndTime();
+// setInterval(updateDateAndTime, 1000);
